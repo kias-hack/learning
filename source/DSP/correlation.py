957bd67992c8c94b64664cc6629b2a408b5b7299
Кириченко Александр <KiAS@gendalf.ru>
2020-07-13 17:01:48 +0300

    добавил реализация ковариации и матриц ковариации и корреляции

--- source/DSP/correlation.py
+++ source/DSP/correlation.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 def convolution(x,y):
 	"""Свретка сигнала"""
@@ -8,13 +9,48 @@ def convolution(x,y):
 	xlen = len(x)
 	sumlen = ylen + xlen
 	#return [sum([(x[i+yi] * y[yi])/sumlen for yi in range(ylen)]) for i in range(xlen-ylen)]
-	return [correlation([x[i+offset] for i in range(ylen)], y) for offset in range(xlen-ylen)]
+	return [corr([x[i+offset] for i in range(ylen)], y) for offset in range(xlen-ylen)]
 
-def correlation(x,y):
+def corr(x,y):
+	"""Корреляция двух векторов - показывает на сколько зависит один вектор от другого значения -1 до 1"""
 	N = len(x)
 	x_mean = sum([i for i in x])/N
 	y_mean = sum([i for i in y])/N
 	return sum([(x[i] - x_mean)*(y[i] - y_mean) for i in range(N)])/math.sqrt(sum([math.pow(x[i] - x_mean, 2) for i in range(N)])*sum([math.pow(y[i] - y_mean, 2) for i in range(N)]))
+
+def matrix_corr(xy):
+	"""Матрица корреляции"""
+	size_matrix = len(xy)
+	z = [[0 for i in range(size_matrix)] for e in range(size_matrix)]
+	
+
+	for row in range(size_matrix):
+		for num in range(size_matrix):
+			z[row][num] = corr(xy[row], xy[num])
+
+	return z
+
+
+def cov(x,y):
+	"""Ковариация"""
+	x_len = len(x)
+	y_len = len(y)
+	x_mean = sum([i for i in x])/x_len
+	y_mean = sum([i for i in y])/y_len
+
+	return sum([(x[i]-x_mean)*(y[i] - y_mean) for i in range(x_len)])/(x_len-1)
+
+def matrix_cov(xy):
+	"""Матрица ковариаций"""
+	size_matrix = len(xy)
+	z = [[0 for i in range(size_matrix)] for e in range(size_matrix)]
+	
+
+	for row in range(size_matrix):
+		for num in range(size_matrix):
+			z[row][num] = cov(xy[row], xy[num])
+
+	return z
 
 size_noise_extens = 32
 y = [1, 2, 4, 2, 3]
@@ -40,14 +76,14 @@ def positive(x):
 
 z = [positive(i) for i in z]
 
-plt.figure(figsize=(9,9))
-plt.subplot(3,1,1)
-plt.plot(range(size_noise_extens,size_noise_extens+len(y)), y, marker=".", color="blue")
-plt.plot(range(len(noise_signal)), noise_signal, marker=".", color="red")
+# plt.figure(figsize=(9,9))
+# plt.subplot(3,1,1)
+# plt.plot(range(size_noise_extens,size_noise_extens+len(y)), y, marker=".", color="blue")
+# plt.plot(range(len(noise_signal)), noise_signal, marker=".", color="red")
 
-plt.subplot(3,1,2)
-x = [i * (len(noise_signal)/(len(noise_signal)-len(y))) for i in range(len(z))]
-plt.plot(x, z, marker=".", color="green")
-plt.grid()
+# plt.subplot(3,1,2)
+# x = [i * (len(noise_signal)/(len(noise_signal)-len(y))) for i in range(len(z))]
+# plt.plot(x, z, marker=".", color="green")
+# plt.grid()
 
-plt.show()
+# plt.show()
